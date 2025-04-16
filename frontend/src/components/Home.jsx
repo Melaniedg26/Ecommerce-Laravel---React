473255500fcbaa1ef8622065f1920f3c98b31a04
Melaniedg26 <melanievm8@gmail.com>
2025-04-15 23:01:41 -0700

    Front

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import LatestProducts from './common/LatestProducts';
+import FeaturedProducts from './common/FeaturedProducts';
+import Hero from './common/Hero';
+import Layout from './common/Layout';
+
+
+
+const Home = () => {
+    return (
+        <>
+            <Layout>
+                <Hero />
+                <LatestProducts />
+                <FeaturedProducts />
+            </Layout>
+
+
+
+        </>
+    )
+}
+
+export default Home
---
+++ frontend/src/components/Shop.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import Layout from './common/Layout'
+import ProductImg from '../assets/images/Mens/eight.jpg';
+import Hero from './common/Hero'
+
+const Shop = () => {
+    return (
+        <Layout>
+            <div className='container'>
+                <nav aria-label="breadcrumb" className='py-4'>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Tienda</li>
+                    </ol>
+                </nav>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        <div className='card shadow border-0 mb-3'>
+                            <div className='card-body p-4'>
+                                <h3 className='mb-3'>Categorias</h3>
+                                <ul>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>Niños</label>
+                                    </li>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>Hombres</label>
+                                    </li>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>Mujeres</label>
+                                    </li>
+                                </ul>
+
+                            </div>
+
+
+                        </div>
+                        <div className='card shadow border-0 mb-3'>
+                            <div className='card-body p-4'>
+                                <h3 className='mb-3'>Marcas</h3>
+                                <ul>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>American Eagle</label>
+                                    </li>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>Levis</label>
+                                    </li>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>Nike</label>
+                                    </li>
+                                    <li className='mb-2'>
+                                        <input type="checkbox" />
+                                        <label htmlFor="" className='ps-2'>Columbia</label>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-9'>
+                        <div className='row pb-5'>
+                        <div className='col-md-4 col-6' >
+                            <div className='product card border-0'>
+                                <div className='card-img'>
+                                    <img src={ProductImg} alt="" className='w-100' />
+                                </div>
+                                <div className='card-body pt-3'>
+                                    <a href="">Camisa de cuadros roja para Hombre</a>
+                                    <div className='price'>
+                                        $350 <span className='text-decoration-line-through'>$500</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-6' >
+                            <div className='product card border-0'>
+                                <div className='card-img'>
+                                    <img src={ProductImg} alt="" className='w-100' />
+                                </div>
+                                <div className='card-body pt-3'>
+                                    <a href="">Camisa de cuadros roja para Hombre</a>
+                                    <div className='price'>
+                                        $350 <span className='text-decoration-line-through'>$500</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-6' >
+                            <div className='product card border-0'>
+                                <div className='card-img'>
+                                    <img src={ProductImg} alt="" className='w-100' />
+                                </div>
+                                <div className='card-body pt-3'>
+                                    <a href="">Camisa de cuadros roja para Hombre</a>
+                                    <div className='price'>
+                                        $350 <span className='text-decoration-line-through'>$500</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-6' >
+                            <div className='product card border-0'>
+                                <div className='card-img'>
+                                    <img src={ProductImg} alt="" className='w-100' />
+                                </div>
+                                <div className='card-body pt-3'>
+                                    <a href="">Camisa de cuadros roja para Hombre</a>
+                                    <div className='price'>
+                                        $350 <span className='text-decoration-line-through'>$500</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-6' >
+                            <div className='product card border-0'>
+                                <div className='card-img'>
+                                    <img src={ProductImg} alt="" className='w-100' />
+                                </div>
+                                <div className='card-body pt-3'>
+                                    <a href="">Camisa de cuadros roja para Hombre</a>
+                                    <div className='price'>
+                                        $350 <span className='text-decoration-line-through'>$500</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-4 col-6' >
+                            <div className='product card border-0'>
+                                <div className='card-img'>
+                                    <img src={ProductImg} alt="" className='w-100' />
+                                </div>
+                                <div className='card-body pt-3'>
+                                    <a href="">Camisa de cuadros roja para Hombre</a>
+                                    <div className='price'>
+                                        $350 <span className='text-decoration-line-through'>$500</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Shop
